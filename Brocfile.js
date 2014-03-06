fd662e4b754d8cc2fb892e395df355539342c481
toran billups <toranb@gmail.com>
2014-03-06 13:56:20 -0600

    working

--- Brocfile.js
+++ Brocfile.js
@@ -1,23 +1,24 @@
 module.exports = function (broccoli) {
-  var filterTemplates = require('broccoli-template')
   var concat = require('broccoli-concat')
+  var filterTemplates = require('broccoli-template')
+  var pickFiles = require('broccoli-static-compiler')
 
   function preprocess (tree) {
     tree = filterTemplates(tree, {
-      extensions: ['hbs', 'handlebars'],
+      extensions: ['handlebars'],
       compileFunction: 'Ember.Handlebars.compile'
     })
     return tree
   }
 
   var sourceTree = broccoli.makeTree('js');
-  var appTemplates = preprocess(sourceTree, {
-    inputFiles: [
-      'templates/*.handlebars'
-    ],
-    outputFile: '/js/dist/tmpls.min.js'
-  });
+  var templates = pickFiles(sourceTree, {
+    srcDir: '/templates/',
+    destDir: '/compiled'
+  })
+  var appTemplates = preprocess(templates);
 
+  var sourceTree = broccoli.makeTree('js');
   var appJs = concat(sourceTree, {
     inputFiles: [
       'vendor/jquery/jquery.js',
@@ -26,7 +27,7 @@ module.exports = function (broccoli) {
       'app.js',
       'other.js'
     ],
-    outputFile: '/js/dist/deps.min.js'
+    outputFile: '/deps.min.js'
   });
 
   return [appJs, appTemplates];
