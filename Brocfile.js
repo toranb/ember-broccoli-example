d1fa48e1c98754a97cb824b7ee9b22d71fae301e
toran billups <toranb@gmail.com>
2014-03-06 21:11:59 -0600

    working coffee example

--- Brocfile.js
+++ Brocfile.js
@@ -1,39 +1,51 @@
 module.exports = function (broccoli) {
-  var concat = require('broccoli-concat')
-  var templateCompiler = require('broccoli-template-compiler')
-  var pickFiles = require('broccoli-static-compiler')
+  var concat = require('broccoli-concat');
+  var templateCompiler = require('broccoli-template-compiler');
+  var pickFiles = require('broccoli-static-compiler');
+  var filterCoffeeScript = require('broccoli-coffee');
+
+  function precompile (tree) {
+    tree = templateCompiler(tree);
+    return tree;
+  }
 
   function preprocess (tree) {
-    tree = templateCompiler(tree);
-    return tree
+    tree = filterCoffeeScript(tree, {
+      bare: true
+    });
+    return tree;
   }
 
   var sourceTree = broccoli.makeTree('js');
+  var app = pickFiles(sourceTree, {
+    srcDir: '/',
+    destDir: '/app'
+  });
+  var appJs = preprocess(app);
+
   var templates = pickFiles(sourceTree, {
     srcDir: '/templates',
     destDir: '/templates'
   })
-  var appTemplates = preprocess(templates);
+  var appTemplates = precompile(templates);
 
-  var sourceTree = broccoli.makeTree('js');
-  var appJs = concat(sourceTree, {
+  var vendorJs = concat(sourceTree, {
     inputFiles: [
       'vendor/jquery/jquery.js',
       'vendor/handlebars/handlebars.min.js',
-      'vendor/ember/ember.js',
-      'app.js',
-      'other.js'
+      'vendor/ember/ember.js'
     ],
-    outputFile: '/deps.min.js'
+    outputFile: '/vendor.min.js'
   });
 
-  var allJS = new broccoli.MergedTree([appJs, appTemplates]);
+  var allJS = new broccoli.MergedTree([vendorJs, appJs, appTemplates]);
   var merged = concat(allJS, {
     inputFiles: [
-      'deps.min.js',
+      'vendor.min.js',
+      'app/*.js',
       'templates/*.js'
     ],
-    outputFile: '/fin.min.js'
+    outputFile: '/deps.min.js'
   });
 
   return [merged];
